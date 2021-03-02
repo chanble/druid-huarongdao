5ad468306994e5d800f95e6007f44ac99218f9a1
chen <chanble_cn@163.com>
2021-03-02 14:57:27 +0800

    添加了点击事件处理

--- src/app.rs
+++ src/app.rs
@@ -1,88 +1,135 @@
 
 use druid::{
     theme, AppLauncher, Color, Data, Lens, LocalizedString, RenderContext, Widget, WidgetExt,
-    WindowDesc, Size
+    WindowDesc, Size, Point, Rect, MouseButton,
 };
+use druid::piet::{FontFamily, ImageFormat, InterpolationMode, Text, TextLayoutBuilder};
 use druid::widget::prelude::*;
 use druid::widget::{Label, Painter, BackgroundBrush, Flex, CrossAxisAlignment};
 
-use num_huarongdao::num_hrd::NumHrd;
+use num_huarongdao::num_hrd::{NumHrd, Direction};
 
 #[derive(Clone, Lens)]
-pub struct AppState {
-    pub hrd_data: Vec<Vec<usize>>,
+pub struct AppData {
+    pub hrd_data: NumHrd,
     pub state: String,
 }
 
-impl Data for AppState {
+impl Data for AppData {
     fn same(&self, other: &Self) -> bool {
         self.hrd_data == other.hrd_data
     }
 }
 
-pub fn build_app() -> impl Widget<AppState> {
-    let display = Label::new(|data: &String, _env: &_| data.clone())
-        .with_text_size(32.0)
-        .lens(AppState::state)
-        .padding(5.0);
-    let mut flex = Flex::column()
-        .with_flex_spacer(0.2)
-        .with_child(display)
-        .with_flex_spacer(0.2)
-        .cross_axis_alignment(CrossAxisAlignment::End);
 
-    flex = flex_vec(flex, vec!(
-        vec!(0, 1, 2,),
-        vec!(3, 4, 5,),
-        vec!(6, 7, 8,),
-    ));
-    flex
+pub struct HuarongDaoWidget {
+    pub cell_size: Size,
 }
 
-fn flex_vec(mut flex: Flex<AppState>, v: Vec<Vec<usize>>) -> Flex<AppState> {
-
-    for r in v {
-        let mut flex_row_data: Vec<Box<dyn Widget<AppState>>> = Vec::new();
-        for c in r {
-            flex_row_data.push(Box::new(digit_button(c)));
+impl HuarongDaoWidget {
+    fn grid_pos(&self, p: Point, grid_size: u8) -> Option<(u8, u8)> {
+        let w0 = self.cell_size.width;
+        let h0 = self.cell_size.height;
+        if p.x < 0.0 || p.y < 0.0 || w0 == 0.0 || h0 == 0.0 {
+            return None;
         }
-        flex.add_flex_child(
-                flex_row(flex_row_data), 1.0,
-            );
-        flex.add_spacer(1.0);
+        let row = (p.x / w0) as u8;
+        let col = (p.y / h0) as u8;
+        if row >= grid_size || col >= grid_size {
+            return None;
+        }
+        Some((row, col))
     }
-    flex
 }
 
-fn flex_row<T: Data>(
-    vw: Vec<Box<dyn Widget<T>>>
-) -> impl Widget<T> {
-    let mut flex = Flex::row();
-    for x in vw {
-        flex = flex.with_flex_child(x, 1.0).with_spacer(1.0);
+impl Widget<AppData> for HuarongDaoWidget {
+
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppData, _env: &Env) {
+        match event {
+            Event::MouseUp(e) => {
+                if e.button == MouseButton::Left {
+                    let pos = e.pos;
+                    println!("MouseUp pos: {}", pos);
+                    if let Some(grid_pos) = self.grid_pos(e.pos, *data.hrd_data.size()) {
+                        data.hrd_data.move_num_by_point(grid_pos);
+                    }
+                }
+            },
+            Event::MouseDown(e) => {
+                let pos = e.pos;
+                // println!("MouseDown pos: {}", pos);
+
+            },
+            Event::MouseMove(e) => {
+                // 
+            },
+            _ => {}
+        }
     }
-    flex
-}
 
+    
+    fn lifecycle(
+        &mut self,
+        _ctx: &mut LifeCycleCtx,
+        _event: &LifeCycle,
+        _data: &AppData,
+        _env: &Env,
+    ) {
+    }
 
-fn digit_button(digit: usize) -> impl Widget<AppState> {
-    let painter = Painter::new(|ctx, _, env| {
-        let bounds = ctx.size().to_rect();
-
-        ctx.fill(bounds, &env.get(theme::BACKGROUND_LIGHT));
-
-        if ctx.is_hot() {
-            ctx.stroke(bounds.inset(-0.5), &Color::WHITE, 1.0);
+    
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &AppData, data: &AppData, _env: &Env) {
+        if data.hrd_data != old_data.hrd_data {
+            ctx.request_paint();
         }
+    }
 
-        if ctx.is_active() {
-            ctx.fill(bounds, &Color::rgb8(0x71, 0x71, 0x71));
+    
+    fn layout(
+        &mut self,
+        _layout_ctx: &mut LayoutCtx,
+        bc: &BoxConstraints,
+        _data: &AppData,
+        _env: &Env,
+    ) -> Size {
+        bc.max()
+    }
+
+    
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &AppData, _env: &Env) {
+        let grid_size = *data.hrd_data.size();
+        let size: Size = ctx.size();
+        let w0 = size.width / grid_size as f64;
+        let h0 = size.height / grid_size as f64;
+        self.cell_size = Size {
+            width: w0,
+            height: h0,
+        };
+        for row in 0..grid_size {
+            for col in 0..grid_size {
+                let txt_num = *data.hrd_data.get_by_point((row, col)).unwrap();
+                let point = Point {
+                    x: w0 * col as f64,
+                    y: h0 * row as f64,
+                };
+                let rect = Rect::from_origin_size(point, self.cell_size);
+                let border = Rect::from_origin_size(point, self.cell_size);
+                
+                ctx.stroke(border, &Color::rgb8(0, 0, 0), 5.0);
+                ctx.fill(rect, &Color::rgb8(0x71, 0x71, 0x71));
+                
+                let rect_center = rect.center();
+                let font_size = 48.0;
+                let half_font_size = font_size / 2.0;
+                let text = ctx.text();
+                let layout = text
+                    .new_text_layout(txt_num.get_n().to_string())
+                    .font(FontFamily::MONOSPACE, font_size)
+                    .text_color(Color::rgb8(128, 0, 0))
+                    .build()
+                    .unwrap();
+                ctx.draw_text(&layout, (rect_center.x - half_font_size, rect_center.y - half_font_size));
+            }
         }
-    });
-
-    Label::new(format!("{}", digit))
-        .with_text_size(24.)
-        .center()
-        .background(painter)
-        .expand()
+    }
 }
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,26 +1,21 @@
 
 use druid::{
     theme, AppLauncher, Color, Data, Lens, LocalizedString, RenderContext, Widget, WidgetExt,
-    WindowDesc,
+    WindowDesc, Size,
 };
+use druid::widget::{Button, Flex, Label, Slider};
 
-use druid_huarongdao::app::{self, AppState};
+use druid_huarongdao::app::{self, AppData, HuarongDaoWidget};
 
 use num_huarongdao::num_hrd::NumHrd;
 
 fn main() {
-    let hrd = NumHrd::new(&1);
-    let mut hrd_data1: Vec<Vec<usize>> = Vec::new();
-    let mut hrd_data_row: Vec<usize> = Vec::new();
-    hrd_data_row.push(0);
-    hrd_data_row.push(1);
-    hrd_data_row.push(2);
-    hrd_data1.push(hrd_data_row);
-    let app_state = AppState {
-        hrd_data: hrd_data1,
+    let hrd = NumHrd::new(&3);
+    let app_state = AppData {
+        hrd_data: hrd,
         state: format!("ddd"),
     };
-    let window = WindowDesc::new(app::build_app)
+    let window = WindowDesc::new(make_widget)
         .window_size((223., 300.))
         .resizable(true)
         .title(
@@ -31,4 +26,19 @@ fn main() {
         .use_simple_logger()
         .launch(app_state)
         .expect("launch failed");
+}
+
+
+fn make_widget() -> impl Widget<AppData> {
+
+    Flex::column()
+        .with_flex_child(
+            HuarongDaoWidget {
+                cell_size: Size {
+                    width: 0.0,
+                    height: 0.0,
+                },
+            },
+            1.0,
+        )
 }
