a9ceeda46d08bebc69e0c3f8fe6e7188598e7475
chen <chanble_cn@163.com>
2021-03-02 21:51:38 +0800

    完成计时功能

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
-
+use std::time::Duration;
 use druid::{
     theme, AppLauncher, Color, Data, Lens, LocalizedString, RenderContext, Widget, WidgetExt,
-    WindowDesc, Size, Point, Rect, MouseButton,
+    WindowDesc, Size, Point, Rect, MouseButton, TimerToken,
 };
 use druid::piet::{FontFamily, ImageFormat, InterpolationMode, Text, TextLayoutBuilder};
 use druid::widget::prelude::*;
@@ -9,21 +9,76 @@ use druid::widget::{Label, Painter, BackgroundBrush, Flex, CrossAxisAlignment};
 
 use num_huarongdao::num_hrd::{NumHrd, Direction};
 
+static TIMER_INTERVAL: Duration = Duration::from_secs(1);
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum State {
+    Preparation,
+    Ready,
+    Running,
+    Finished,
+}
+
 #[derive(Clone, Lens)]
 pub struct AppData {
     pub hrd_data: NumHrd,
-    pub state: String,
+    win: bool,
+    duration: String,
+    steps: String,
+    tip: String,
+    pub state: State,
 }
 
 impl Data for AppData {
     fn same(&self, other: &Self) -> bool {
         self.hrd_data == other.hrd_data
+        && self.win == other.win
+        && self.duration == other.duration
+        && self.steps == other.steps
+        && self.tip == other.tip
     }
 }
 
+impl AppData {
+
+    pub fn new(hrd: NumHrd) -> Self {
+        Self {
+            hrd_data: hrd,
+            win: false,
+            duration: format!("0"),
+            steps: format!("0"),
+            tip: format!(""),
+            state: State::Preparation,
+        }
+    }
+    pub fn set_win(&mut self) {
+        self.win = true;
+        self.tip = format!("恭喜您！");
+        self.state = State::Finished;
+    }
+
+    pub fn incr_duration(&mut self) {
+        let d_usize = self.duration.parse::<usize>().unwrap();
+        self.duration = format!("{}", d_usize + 1);
+    }
+
+    pub fn incr_step(&mut self) {
+        let d_usize = self.steps.parse::<usize>().unwrap();
+        self.steps = format!("{}", d_usize + 1);
+    }
+
+    pub fn start(&mut self) {
+        self.steps = format!("0");
+        self.tip = format!("");
+        self.duration = format!("0");
+        self.win = false;
+        self.state = State::Running;
+    }
+}
 
 pub struct HuarongDaoWidget {
     pub cell_size: Size,
+    pub timer_id: TimerToken,
 }
 
 impl HuarongDaoWidget {
@@ -38,7 +93,7 @@ impl HuarongDaoWidget {
         if row >= grid_size || col >= grid_size {
             return None;
         }
-        Some((row, col))
+        Some((col, row))
     }
 }
 
@@ -46,13 +101,27 @@ impl Widget<AppData> for HuarongDaoWidget {
 
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppData, _env: &Env) {
         match event {
+            Event::WindowConnected => {
+                // Start the timer when the application launches
+            }
             Event::MouseUp(e) => {
                 if e.button == MouseButton::Left {
                     let pos = e.pos;
                     println!("MouseUp pos: {}", pos);
                     if let Some(grid_pos) = self.grid_pos(e.pos, *data.hrd_data.size()) {
+                        println!("grid pos: {:?}", grid_pos);
                         data.hrd_data.move_num_by_point(grid_pos);
                     }
+                    if data.hrd_data.is_win() {
+                        data.set_win();
+                    }
+                }
+            },
+            Event::Timer(id) => {
+                // println!("event timer id: {:?}, timer_id: {:?}");
+                if *id == self.timer_id && data.state == State::Running{
+                    data.incr_duration();
+                    self.timer_id = ctx.request_timer(TIMER_INTERVAL);
                 }
             },
             Event::MouseDown(e) => {
@@ -79,8 +148,13 @@ impl Widget<AppData> for HuarongDaoWidget {
 
     
     fn update(&mut self, ctx: &mut UpdateCtx, old_data: &AppData, data: &AppData, _env: &Env) {
-        if data.hrd_data != old_data.hrd_data {
+        if !data.same(old_data) {
             ctx.request_paint();
+        }
+
+        println!("data.state: {:?}", data.state);
+        if data.state == State::Running {
+            self.timer_id = ctx.request_timer(TIMER_INTERVAL);
         }
     }
 
@@ -107,7 +181,7 @@ impl Widget<AppData> for HuarongDaoWidget {
         };
         for row in 0..grid_size {
             for col in 0..grid_size {
-                let txt_num = *data.hrd_data.get_by_point((row, col)).unwrap();
+                let txt_num = *data.hrd_data.num_by_point((row, col)).unwrap();
                 let point = Point {
                     x: w0 * col as f64,
                     y: h0 * row as f64,
@@ -117,18 +191,20 @@ impl Widget<AppData> for HuarongDaoWidget {
                 
                 ctx.stroke(border, &Color::rgb8(0, 0, 0), 5.0);
                 ctx.fill(rect, &Color::rgb8(0x71, 0x71, 0x71));
-                
-                let rect_center = rect.center();
-                let font_size = 48.0;
-                let half_font_size = font_size / 2.0;
-                let text = ctx.text();
-                let layout = text
-                    .new_text_layout(txt_num.get_n().to_string())
-                    .font(FontFamily::MONOSPACE, font_size)
-                    .text_color(Color::rgb8(128, 0, 0))
-                    .build()
-                    .unwrap();
-                ctx.draw_text(&layout, (rect_center.x - half_font_size, rect_center.y - half_font_size));
+                let txt_num_n = txt_num.get_n();
+                if txt_num_n > 0 {
+                    let rect_center = rect.center();
+                    let font_size = 48.0;
+                    let half_font_size = font_size / 2.0;
+                    let text = ctx.text();
+                    let layout = text
+                        .new_text_layout(txt_num_n.to_string())
+                        .font(FontFamily::MONOSPACE, font_size)
+                        .text_color(Color::rgb8(128, 0, 0))
+                        .build()
+                        .unwrap();
+                    ctx.draw_text(&layout, (rect_center.x - half_font_size, rect_center.y - half_font_size));
+                }
             }
         }
     }
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,23 @@
 
 use druid::{
     theme, AppLauncher, Color, Data, Lens, LocalizedString, RenderContext, Widget, WidgetExt,
-    WindowDesc, Size,
+    WindowDesc, Size, TimerToken,
 };
 use druid::widget::{Button, Flex, Label, Slider};
+use rand::prelude::*;
+use std::time::{SystemTime, UNIX_EPOCH};
+use druid_huarongdao::app::{self, AppData, HuarongDaoWidget, State};
 
-use druid_huarongdao::app::{self, AppData, HuarongDaoWidget};
-
-use num_huarongdao::num_hrd::NumHrd;
+use num_huarongdao::num_hrd::{NumHrd, Direction};
 
 fn main() {
     let hrd = NumHrd::new(&3);
-    let app_state = AppData {
-        hrd_data: hrd,
-        state: format!("ddd"),
-    };
+    let app_state = AppData::new(hrd);
     let window = WindowDesc::new(make_widget)
         .window_size((223., 300.))
         .resizable(true)
         .title(
-            LocalizedString::new("calc-demo-window-title").with_placeholder("Simple Calculator"),
+            LocalizedString::new("fifteen-puzzle-window-title").with_placeholder("数字华容道-- by chanble_cn@163.com"),
         );
 
     AppLauncher::with_window(window)
@@ -33,12 +31,56 @@ fn make_widget() -> impl Widget<AppData> {
 
     Flex::column()
         .with_flex_child(
+            Label::new("数字华容道")
+                .with_text_size(52.0)
+                .padding(5.0),
+            1.0,
+        )
+        .with_child(
+            Flex::row()
+            .with_child(Label::new("用时："))
+            .with_child(Label::new(|data: &String, _env: &_| data.clone()).lens(AppData::duration))
+            .with_child(Label::new("步数："))
+            .with_child(Label::new(|data: &String, _env: &_| data.clone()).lens(AppData::steps))
+            
+        )
+        .with_flex_child(
             HuarongDaoWidget {
+                timer_id: TimerToken::INVALID,
                 cell_size: Size {
                     width: 0.0,
                     height: 0.0,
                 },
             },
             1.0,
+        )
+        .with_child(
+            Button::new("开始游戏").on_click(|_ctx, data: &mut AppData, _env| {
+                for i in 0..500 {
+                    let mut rng = rand::thread_rng();
+                    let r = rng.gen_range(0..4);
+                    let direction = match r {
+                        0 => Direction::Top,
+                        1 => Direction::Bottom,
+                        2 => Direction::Left,
+                        _ => Direction::Right,
+                    };
+                    data.hrd_data.zero_move(&direction).unwrap();
+                }
+                println!("shuffle over");
+                data.incr_step();
+                data.state = State::Ready;
+                if data.state == State::Ready {
+                    data.start();
+                }
+                _ctx.request_paint();
+            }),
+        )
+        .with_child(
+            Label::new(|data: &String, _env: &_| data.clone())
+                .with_text_size(48.0)
+                .with_text_color(Color::rgb8(0xf0, 0xf0, 0xea))
+                .padding(5.0)
+                .lens(AppData::tip)
         )
 }
